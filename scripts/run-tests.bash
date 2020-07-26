f659debd449f525917e93516858655ede9291647
John Stilia <stilia.johny@gmail.com>
2020-07-26 21:16:52 +0100

    add pre-commit hook ansible lint

---
+++ scripts/run-tests.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# if any command inside script returns error, exit and return that error 
+set -e
+
+# magic line to ensure that we're always inside the root of our application,
+# no matter from which directory we'll run script
+# thanks to it we can just enter `./scripts/run-tests.bash`
+cd "${0%/*}/.."
+
+echo "Running tests"
+echo "............................" 
+ansible-lint *.yml --force-color -p -x 204,701,301,305,401,303
+
