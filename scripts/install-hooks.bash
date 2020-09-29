50899f0b7ff5a42492499dd822da116dca2f030e
John Stilia <stilia.johny@gmail.com>
2020-09-29 21:28:32 +0100

    remove some roles.old, modify pre-commit hook split vagrant tool

--- scripts/install-hooks.bash
+++ scripts/install-hooks.bash
@@ -1,4 +1,4 @@
-T_DIR=$(git rev-parse --git-dir)
+GIT_DIR=$(git rev-parse --git-dir)
 
 echo "Installing hooks..."
 # this command creates symlink to our pre-commit script
--- scripts/run-tests.bash
+++ scripts/run-tests.bash
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
+
 # if any command inside script returns error, exit and return that error
 set -e
 
-# magic line to ensure that we're always inside the root of our application,
-# no matter from which directory we'll run script
-# thanks to it we can just enter `./scripts/run-tests.bash`
-cd "${0%/*}/.."
+if ! command -v ansible-lint &> /dev/null
+then
+	echo -e "ansible-lint could not be found"
+    echo -e "Consider installing ansible lint with: pip install --user ansible-lint"
+	exit
+else command -v ansible-lint &> /dev/null
 
-echo "Running ansible lint on *.yml on git root"
-echo "............................"
-ansible-lint *.yml --force-color -p -x 204,701,301,305,401,303
+	# magic line to ensure that we're always inside the root of our application,
+	# no matter from which directory we'll run script
+	# thanks to it we can just enter `./scripts/run-tests.bash`
+	cd "${0%/*}/.."
+	
+	echo "Running ansible lint on *.yml on git root"
+	echo "............................"
+	ansible-lint *.yml --force-color -p -x 204,701,301,305,401,303
 
+
+fi
