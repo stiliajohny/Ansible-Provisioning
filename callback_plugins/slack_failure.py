ad1d53d2d71311939ad82e63a1a6fcf0fb6ad69d
John Stilia <stilia.johny@gmail.com>
2020-12-04 00:11:59 +0000

    small restructuring

--- callback_plugins/slack_failure.py
+++ callback_plugins/slack_failure.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: iso-8859-15 -*-
 from __future__ import (absolute_import, division, print_function)
 from ansible.plugins.callback import CallbackBase
@@ -10,13 +10,12 @@ import urllib3
 import sys
 import os
 import requests
+import os
 
+myhost = os.uname()[1]
 http = urllib3.PoolManager()
 
 
-
-# Ansible documentation of the module. I'm also not sure why this is required,
-# but other plugins add documentation so it seems to be a standard.
 DOCUMENTATION = '''
     callback: slack
     options:
@@ -30,6 +29,7 @@ DOCUMENTATION = '''
           - name: SLACK_CHANNEL
 '''
 
+
 class CallbackModule(CallbackBase):
     CALLBACK_VERSION = 2.0
     CALLBACK_NAME = 'slack_failure'
@@ -39,7 +39,8 @@ class CallbackModule(CallbackBase):
         super(CallbackModule, self).__init__()
 
     def set_options(self, task_keys=None, var_options=None, direct=None):
-        super(CallbackModule, self).set_options(task_keys=task_keys, var_options=var_options, direct=direct)
+        super(CallbackModule, self).set_options(
+            task_keys=task_keys, var_options=var_options, direct=direct)
 
         # Read and assign environment variables to memory so that we can use
         # them later.
@@ -47,7 +48,12 @@ class CallbackModule(CallbackBase):
         self.slack_channel = os.environ.get('SLACK_CHANNEL')
 
         if self.slack_webhook_url is None:
-            self._display.display('Error: The slack callback plugin requires `SLACK_WEBHOOK_URL` to be defined in the environment')
+            self._display.display(
+                'Error: The slack callback plugin requires `SLACK_WEBHOOK_URL` to be defined in the environment')
+            sys.exit(1)
+        if self.slack_channel is None:
+            self._display.display(
+                'Error: The slack callback plugin requires `SLACK_CHANNEL` to be defined in the environment')
             sys.exit(1)
 
     def v2_runner_on_failed(self, taskResult, ignore_errors=False):
@@ -58,23 +64,19 @@ class CallbackModule(CallbackBase):
 
 
 def notify(slack_webhook_url, taskResult, slack_channel=None):
-    # Format the Slack message. We'll use message attachments
-    # https://api.slack.com/docs/message-attachments
     payload = {
         'username': 'Ansible',
         'attachments': [
             {
-                'title': 'Ansible run has failed. HOST: {} {}'.format(taskResult._host, taskResult._task),
+                'title': 'Ansible run has failed.\n HOST: {}\n {}'.format(myhost, taskResult._task),
                 'color': '#FF0000',
                 'text': '```{}```'.format(json.dumps(taskResult._result, indent=4))
             }
         ]
     }
 
-    # The webhook has a default url. If one is not configured, we'll use the
-    # default
     if slack_channel:
         payload['channel'] = slack_channel
 
-    print(payload)
-    response = requests.post( slack_webhook_url, data=json.dumps(payload), headers={'Content-Type': 'application/json'})
+    response = requests.post(slack_webhook_url, data=json.dumps(
+        payload), headers={'Content-Type': 'application/json'})
